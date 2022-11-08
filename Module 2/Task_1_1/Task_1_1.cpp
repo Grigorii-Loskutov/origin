db17cd5d111990c70a5793cb4e1803fced41de88
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2022-11-08 23:43:49 +0300

    Create Task_1_1.cpp

---
+++ Module 2/Task_1_1/Task_1_1.cpp
@@ -0,0 +1,25 @@
+﻿// Task_1_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <windows.h>
+
+unsigned arr_size = 0;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::string size_of_arr_str, cells_str;
+    std::ifstream fin("in.txt");
+    if (fin.is_open()) {
+        fin >> arr_size;
+        std::cout << arr_size;
+    }
+    else {
+        std::cout << "Не получилось открыть файл!";
+    }
+}
