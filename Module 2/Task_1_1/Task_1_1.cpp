49d65974bea933dbb31f5db4757aefa878c61a22
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2022-11-09 23:30:36 +0300

    Update Task_1_1.cpp
    work revision

--- Module 2/Task_1_1/Task_1_1.cpp
+++ Module 2/Task_1_1/Task_1_1.cpp
@@ -8,16 +8,47 @@
 #include <string>
 #include <windows.h>
 
-unsigned arr_size = 0;
+//int* create_int_array(unsigned size = 0) {
+//    int* arr = new int [size];
+//    return arr;
+//}
+
+unsigned arr1_size = 0, arr2_size = 0;
+std::string arr_str;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    std::string size_of_arr_str, cells_str;
+    setlocale(LC_ALL, "Russian");   
     std::ifstream fin("in.txt");
     if (fin.is_open()) {
-        fin >> arr_size;
-        std::cout << arr_size;
+        fin >> arr1_size;     
+        int* arr1 = new int[arr1_size];
+        for (int iter = 0; iter < arr1_size; ++iter) {
+            fin >> arr1[iter];
+            std::cout << arr1[iter] << " ";
+        }
+        std::cout << "\n";
+        fin >> arr2_size;
+        int* arr2 = new int[arr2_size];
+        for (int iter = 0; iter < arr2_size; ++iter) {
+            fin >> arr2[iter];
+            std::cout << arr2[iter] << " ";
+        }
+        std::ofstream fout("out.txt");
+        if (fout.is_open()) {
+            fout << arr2_size << "\n";
+            fout << arr2[arr2_size - 1] << " ";
+            for (unsigned iter = 0; iter < arr2_size - 1; ++iter) {
+                fout << arr2[iter] << " ";
+            }
+            fout << "\n";
+            fout << arr1_size << "\n";
+            for (unsigned iter = 1; iter < arr1_size; ++iter) {
+                fout << arr1[iter] << " ";
+            }
+            fout << arr1[0];
+            fout.close();
+        }
     }
     else {
         std::cout << "Не получилось открыть файл!";
