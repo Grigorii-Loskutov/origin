5daed4280b2ab3dd515268c3c4246a0b37343439
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2022-11-30 22:46:48 +0300

    Update Task_5_2.cpp

--- Module 2/Task_5_2/Task_5_2.cpp
+++ Module 2/Task_5_2/Task_5_2.cpp
@@ -64,7 +64,7 @@ public:
 
 class Quadrangle :public Figure {
 public:
-	Quadrangle() : Figure(3, "Default Quadrangle", 1, 1, 1, 1, 90, 90, 90, 90) {}
+	Quadrangle() : Figure(4, "Default Quadrangle", 1, 1, 1, 1, 90, 90, 90, 90) {}
 	Quadrangle(std::string name, double a, double b, double c, double d, double A, double B, double C, double D) : Figure(4, name, a, b, c, d, A, B, C, D) {}
 
 };
