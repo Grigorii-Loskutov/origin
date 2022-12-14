fb4cdddbf46a15d29901b4f430969d4eda900671
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2022-12-14 22:39:59 +0300

    to UTF-8

--- Module 2/Task_8_2/Equilateral_Triangle.cpp
+++ Module 2/Task_8_2/Equilateral_Triangle.cpp
@@ -4,19 +4,19 @@
 #include "Figure_Creation_Except.h"
 #include "Equilateral_Triangle.h"
 
-Equilateral_Triangle::Equilateral_Triangle(double a, double b, double c, double A, double B, double C) : Triangle("–авносторонний треугольник", a, b, c, A, B, C) {
+Equilateral_Triangle::Equilateral_Triangle(double a, double b, double c, double A, double B, double C) : Triangle("Равносторонний треугольник", a, b, c, A, B, C) {
 	if ((A != 60) or (B != 60) or (C != 60)) {
 		std::stringstream ss;
 		ss << "\n";
 		ss << name << "(стороны " << a << ", " << b << ", " << c << "; углы " << A << ", " << B << ", " << C << ") не был создан. ";
-		ss << "ѕричина: один из углов не равен 60 градусов";
+		ss << "Причина: один из углов не равен 60 градусов";
 		throw Figure_Creation_Except(ss.str());
 	}
 	if ((c != a) or (c != b) or (a != b)) {
 		std::stringstream ss;
 		ss << "\n";
 		ss << name << "(стороны " << a << ", " << b << ", " << c << "; углы " << A << ", " << B << ", " << C << ") не был создан. ";
-		ss << "ѕричина: стороны не равны между собой";
+		ss << "Причина: стороны не равны между собой";
 		throw Figure_Creation_Except(ss.str());
 	}
 }
--- Module 2/Task_8_2/Square.cpp
+++ Module 2/Task_8_2/Square.cpp
@@ -1,22 +1,22 @@
-#include <string.h>
+п»ї#include <string.h>
 #include <iostream>
 #include <sstream>
 #include "Figure_Creation_Except.h"
 #include "Square.h"
 
-Square::Square(double a, double b, double c, double d, double A, double B, double C, double D) : Quadrangle("Квадрат", a, a, a, a, 90, 90, 90, 90) {
+Square::Square(double a, double b, double c, double d, double A, double B, double C, double D) : Quadrangle("РљРІР°РґСЂР°С‚", a, a, a, a, 90, 90, 90, 90) {
 	if ((A != 90) or (B != 90) or (C != 90) or (D != 90)) {
 		std::stringstream ss;
 		ss << "\n";
-		ss << name << "(стороны " << a << ", " << b << ", " << c << ", " << d << "; углы " << A << ", " << B << ", " << C << ", " << D << ") не был создан. ";
-		ss << "Причина: один из углов не равен 90 гр.";
+		ss << name << "(СЃС‚РѕСЂРѕРЅС‹ " << a << ", " << b << ", " << c << ", " << d << "; СѓРіР»С‹ " << A << ", " << B << ", " << C << ", " << D << ") РЅРµ Р±С‹Р» СЃРѕР·РґР°РЅ. ";
+		ss << "РџСЂРёС‡РёРЅР°: РѕРґРёРЅ РёР· СѓРіР»РѕРІ РЅРµ СЂР°РІРµРЅ 90 РіСЂ.";
 		throw Figure_Creation_Except(ss.str());
 	}
 	if ((a != c) or (a != b) or (a != c) or (a != d)) {
 		std::stringstream ss;
 		ss << "\n";
-		ss << name << "(стороны " << a << ", " << b << ", " << c << ", " << d << "; углы " << A << ", " << B << ", " << C << ", " << D << ") не был создан. ";
-		ss << "Причина: стороны не равны между собой";
+		ss << name << "(СЃС‚РѕСЂРѕРЅС‹ " << a << ", " << b << ", " << c << ", " << d << "; СѓРіР»С‹ " << A << ", " << B << ", " << C << ", " << D << ") РЅРµ Р±С‹Р» СЃРѕР·РґР°РЅ. ";
+		ss << "РџСЂРёС‡РёРЅР°: СЃС‚РѕСЂРѕРЅС‹ РЅРµ СЂР°РІРЅС‹ РјРµР¶РґСѓ СЃРѕР±РѕР№";
 		throw Figure_Creation_Except(ss.str());
 	}
 }
