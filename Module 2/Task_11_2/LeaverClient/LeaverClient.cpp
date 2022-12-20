d7c89eff98e4c7283efad26332b12db3adcb7e2e
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2022-12-20 23:08:50 +0300

    Initial commite

---
+++ Module 2/Task_11_2/LeaverClient/LeaverClient.cpp
@@ -0,0 +1,20 @@
+﻿// LeaverClient.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include "Leaver.h"
+#include <windows.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Leaver::Leaver example;
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::cin >> name;
+    name = example.leaver(name) + "!";
+    std::cout << name;
+}
