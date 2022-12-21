c0a88207bf489a832e1cc6ee1cf646c6e59bc66c
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2022-12-21 21:51:32 +0300

    Initial commite

---
+++ Module 2/Task_11_3/Leaver.cpp
@@ -0,0 +1,8 @@
+﻿#include <string>
+#include "Leaver.h"
+
+namespace Leaver {
+	std::string Leaver::leaver(std::string name) {
+		return "До свидания, " + name;
+	}
+}
---
+++ Module 2/Task_11_3/Leaver.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <string>
+
+#ifdef LEAVERLIBRARYDYNAMIC_EXPORTS
+#define LEAVERLIBRARY_API __declspec(dllexport)
+#else
+#define LEAVERLIBRARY_API __declspec(dllimport)
+#endif
+
+namespace Leaver
+{
+	class Leaver
+	{
+	public:
+		LEAVERLIBRARY_API std::string leaver (std::string name);
+	};
+}
