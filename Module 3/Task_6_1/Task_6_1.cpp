ee9d6a7e82fe3f5090fa5b718fd54209ee9ed8b0
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2023-01-29 00:08:44 +0300

    Init commite

---
+++ Module 3/Task_6_1/Task_6_1.cpp
@@ -0,0 +1,29 @@
+﻿// Task_6_1.cpp : Задача 1. Наивная хэш-функция для строки
+//https://github.com/netology-code/algocpp-homeworks/tree/main/6/01
+
+#include <iostream>
+#include <string.h>
+#include <windows.h>
+
+int simple_string_hash(char* str, int* length) {
+	int summ = 0;
+	for (int iter = 0; iter < *length; iter++) {
+		summ += str[iter];
+	}
+	return summ;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::string str;
+	do {
+		std::cout << "Введите строку: "; std::cin >> str;
+		char* char_arr = &str[0];
+		int length = str.length();
+		std::cout << "Наивный хэш строки " << str << " = " << simple_string_hash(char_arr, &length) << "\n";
+	} 
+	while (str!="exit");
+}
