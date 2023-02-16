6865d5ecbcf10409301db41c7a673b6de0525d7c
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2023-02-16 22:24:58 +0300

    Initial commite
    Пока без лямбда-функций

---
+++ Module 4/Task_1_3/Task_1_3.cpp
@@ -0,0 +1,37 @@
+﻿// Task_1_3.cpp :  Задача 3*. Операции над углами
+// https://github.com/netology-code/cppl-homeworks/tree/main/01/03
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <windows.h>
+
+void sinus(double angel) {
+	std::cout << " sin: " << std::sin(3.1415926 * angel / 180);
+}
+
+void cosinus(double angel) {
+	std::cout << " cos: " << std::cos(3.1415926 * angel / 180);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::vector<double> angles = { 0, 30, 45, 90 };
+	//[](double angel) {std::cout << "sin: " << std::sin(3.1415926 * angel / 180); };
+	//[](double angel) {std::cout << "cos: " << std::cos(3.1415926 * angel / 180); };
+	std::vector < std::function<void(double)>> functions;
+	functions.emplace_back(sinus);
+	functions.emplace_back(cosinus);
+	for (const auto& angle : angles) {
+		std::cout << angle << ": ";
+		for (const auto& function : functions)
+			function(angle);
+		std::cout << std::endl;
+	}
+}
+
