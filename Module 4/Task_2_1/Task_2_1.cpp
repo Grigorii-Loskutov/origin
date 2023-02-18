138c2c69b9917485f6846e6a5002ab44cc620211
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2023-02-18 16:12:06 +0300

    Init commite

---
+++ Module 4/Task_2_1/Task_2_1.cpp
@@ -0,0 +1,29 @@
+﻿// Task_2_1.cpp : Задача 1. Программа с ошибками
+// https://github.com/netology-code/cppl-homeworks/tree/main/02/01
+
+#include <iostream>
+using namespace std;
+
+struct point {
+public:
+	double m_x;
+	double m_y;
+	point(double x, double y) {
+		this->m_x = x;
+		this->m_y = y;
+	}
+};
+
+void print_point(const point& point_object) {
+	std::cout << "x:" << point_object.m_x << ", y: " << point_object.m_y << std::endl;
+}
+
+int main()
+{
+	int i;
+	for (i = 0; i < 5; i++) {
+		point my_point(i, 2 * i);
+		print_point(my_point);
+	}
+	return 0;
+}
