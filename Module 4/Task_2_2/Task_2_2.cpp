f3ebd4805dd965dde7f8e294beb99248e2a9824b
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2023-02-22 19:48:51 +0300

    Init commite

---
+++ Module 4/Task_2_2/Task_2_2.cpp
@@ -0,0 +1,9 @@
+﻿// Task_2_2.cpp : Задача 2*. Сплошные ошибки
+// https://github.com/netology-code/cppl-homeworks/tree/main/02/02
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
