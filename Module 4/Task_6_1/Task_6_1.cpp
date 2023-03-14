e7ab65861aa8c7f96e70794ff52e2a0125b45aee
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2023-03-14 22:58:36 +0300

    Init commite

---
+++ Module 4/Task_6_1/Task_6_1.cpp
@@ -0,0 +1,9 @@
+﻿// Task_6_1.cpp : Задача 1. Частота символов
+// https://github.com/netology-code/cppl-homeworks/tree/main/07/01
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
