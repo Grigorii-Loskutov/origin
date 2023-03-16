9775121f462359d81dae3523898a8e53be501f6d
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2023-03-16 21:47:17 +0300

    Успех

--- Module 4/Task_6_1/Task_6_1.cpp
+++ Module 4/Task_6_1/Task_6_1.cpp
@@ -2,8 +2,40 @@
 // https://github.com/netology-code/cppl-homeworks/tree/main/07/01
 
 #include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <windows.h>
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russia");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::string str = "Hello world!!";
+	std::map<char, int> keyAndMap;
+	for (const auto iter : str) {
+		keyAndMap[iter] += 1;
+	}
+	for (const auto& n : keyAndMap) {
+		std::cout << n.first << ": " << n.second << "\n";
+	}
+	std::cout << "____________________\n";
+	//Честно подсмотренно на stackoverflow
+	std::set<std::pair<int, char>> s;
+	for (auto const& kv : keyAndMap) {
+		s.emplace(kv.second, kv.first);
+	}
+
+	auto print = [](auto& n) { std::cout << n.first << ": " << n.second << "\n"; };
+	std::for_each(s.rbegin(), s.rend(), print);
+
+	/*for (auto const& vk : s) {
+		std::cout << vk.first << ": " << vk.second << "\n";
+	}*/
+	
+
+	
 }
