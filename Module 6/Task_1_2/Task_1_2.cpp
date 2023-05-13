357d8a415ae342ed8998f9fb5b0a604ca5553eae
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2023-05-13 18:02:08 +0300

    finish

--- Module 6/Task_1_2/Task_1_2.cpp
+++ Module 6/Task_1_2/Task_1_2.cpp
@@ -8,7 +8,8 @@
 #include <chrono>
 #include <thread>
 #include <omp.h>
-
+#include <mutex>
+std::mutex mtx;
 std::vector<int> random_vector(int size) {
     srand(time(nullptr));
     int range = 100;
@@ -31,6 +32,14 @@ std::vector<int> add_vectors(const std::vector<int>& a, const std::vector<int>& 
     return c;
 }
 
+void sum_vectors(const std::vector<int>& a, const std::vector<int>& b, std::vector<int>& result, int start, int end) {
+    for (int i = start; i < end; ++i) {
+        mtx.lock();
+        result[i] = a[i] + b[i];
+        mtx.unlock();
+    }
+}
+
 int main(int argc, char** argv)
 {	
 	setlocale(LC_ALL, "Russia");
@@ -45,13 +54,21 @@ int main(int argc, char** argv)
     for (int range_iter = low; range_iter <= high; range_iter *= 10) {
         std::vector<int> v1 = random_vector(range_iter);
         std::vector<int> v2 = random_vector(range_iter);
+        std::vector<int> c(range_iter);
         std::cout << range_iter << "\t\t";
         std::vector<int> results_1dim;
         for (int num_threads = 1; num_threads <= CPU_number; num_threads *= 2) {
             auto start_time = std::chrono::high_resolution_clock::now();
-
-            std::vector<int> c = add_vectors(v1, v2, num_threads);
-
+            std::vector<std::thread> threads;
+            for (int i = 0; i < num_threads; ++i) {
+                int start = i * range_iter / num_threads;
+                int end = (i + 1) * range_iter / num_threads;
+               
+                threads.emplace_back(sum_vectors, std::ref(v1), std::ref(v2), std::ref(c), start, end);
+            }
+           for (auto& thread : threads) {
+                thread.join();
+            }
             auto end_time = std::chrono::high_resolution_clock::now();
             double duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
 
@@ -60,18 +77,18 @@ int main(int argc, char** argv)
         results_2dim.push_back(results_1dim);
     }
     std::cout << std::endl;
-    std::vector<std::vector<int>> results_2dim_tr(results_2dim[0].size(), std::vector<int>(results_2dim.size()));
+ /*   std::vector<std::vector<int>> results_2dim_tr(results_2dim[0].size(), std::vector<int>(results_2dim.size()));
     std::transform(results_2dim.begin(), results_2dim.end(), results_2dim_tr.begin(),
         [](const std::vector<int>& row) {
             return std::vector<int>(row.begin(), row.end());
-        });
-    int treads = 1;
-    for (int i = 0; i < results_2dim[0].size(); i++) {
-        std::cout << treads << " threads:\t";
+        });*/
+    int thread_num = 1;
+    for (int i = results_2dim[0].size()-1; i >=0; i--) {
+        std::cout << thread_num << " threads:\t";
         for (int j = 0; j < results_2dim.size(); j++) {
             std::cout << results_2dim[j][i] << "\t\t";
         }
         std::cout << std::endl;
-        treads *= 2;
+        thread_num *= 2;
     }
 }
