8a16926b29833f6d980c5ee6940a2c2fedf46ef3
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2023-06-25 19:30:30 +0300

    Init

---
+++ Module 7/TasK2_2/TasK2_2.cpp
@@ -0,0 +1,58 @@
+﻿// TasK2_2.cpp : Домашнее задание к занятию «Структурные шаблоны: Proxy, Decorator, Adapter». Задание 2
+// https://github.com/netology-code/dpcpp-homeworks/tree/main/02#readme
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <windows.h>
+
+class VeryHeavyDatabase {
+public:
+    std::string GetData(const std::string& key) const noexcept {
+        return "Very Big Data String: " + key;
+    }
+};
+
+class CacheProxyDB : VeryHeavyDatabase {
+public:
+    explicit CacheProxyDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        if (cache_.find(key) == cache_.end()) {
+            std::cout << "Get from real object\n";
+            cache_[key] = real_db_->GetData(key);
+        }
+        else {
+            std::cout << "Get from cache\n";
+        }
+        return cache_.at(key);
+    }
+
+private:
+    std::map<std::string, std::string> cache_;
+    VeryHeavyDatabase* real_db_;
+};
+
+class TestDB : VeryHeavyDatabase {
+public:
+    explicit TestDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        return "test_data\n";
+    }
+private:
+    VeryHeavyDatabase* real_db_;
+};
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "Russia");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    auto real_db = VeryHeavyDatabase();
+    auto cached_db = CacheProxyDB(std::addressof(real_db));
+    auto test_db = TestDB(std::addressof(real_db));
+    std::cout << real_db.GetData("key") << std::endl;
+    std::cout << cached_db.GetData("key") << std::endl;
+    std::cout << test_db.GetData("key") << std::endl;
+    return 0;
+}
+
