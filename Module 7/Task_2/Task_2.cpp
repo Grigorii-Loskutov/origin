39e4f605efb5ca9d219b459affed9395bb9b6799
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2023-06-25 17:57:31 +0300

    Init

---
+++ Module 7/Task_2/Task_2.cpp
@@ -0,0 +1,49 @@
+﻿// Task_2.cpp : Домашнее задание к занятию «Структурные шаблоны: Proxy, Decorator, Adapter»
+// https://github.com/netology-code/dpcpp-homeworks/tree/main/02#readme
+
+#include <iostream>
+#include <iostream>
+#include <windows.h>
+
+class Text {
+public:
+	virtual void render(const std::string& data) const {
+		std::cout << data;
+	}
+};
+
+class DecoratedText : public Text {
+public:
+	explicit DecoratedText(Text* text) : text_(text) {}
+	Text* text_;
+};
+
+class ItalicText : public DecoratedText {
+public:
+	explicit ItalicText(Text* text) : DecoratedText(text) {}
+	void render(const std::string& data)  const {
+		std::cout << "<i>";
+		text_->render(data);
+		std::cout << "</i>";
+	}
+};
+
+class BoldText : public DecoratedText {
+public:
+	explicit BoldText(Text* text) : DecoratedText(text) {}
+	void render(const std::string& data) const {
+		std::cout << "<b>";
+		text_->render(data);
+		std::cout << "</b>";
+	}
+};
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "Russia");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	auto text_block = new ItalicText(new BoldText(new Text()));
+	text_block->render("Hello world");
+}
+
